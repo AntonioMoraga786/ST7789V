1861d820b1d3fc74a32ece9c075c8f65166e7a7b
Antonio Moraga <antomoraga7@gmail.com>
2025-07-13 16:16:59 +0100

    define pins to be used in pico

--- code.c
+++ code.c
@@ -1,1 +1,17 @@
+// code for the st7789V tft screen using a 4 wire serial comunication
+// use SPI 0
+
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+#define SPI_PORT spi0 // spi port to use for communication
+#define MISO 0// MISO Pin (Recieve pin in pico)
+#define MOSI 3// MOSI Pin (Transmit pin in pico)
+#define SCK 2 // Clock output of SPI
+#define CS 1// Chip Select pin
+#define DC 4// Data/Command Pin
+
+void main() {
+    
+}
